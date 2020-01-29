7cc5f27664697d2af38e225ea221b3355212c5f4
mizoguchi kenta <347lionz@gmail.com>
2020-01-29 20:44:36 +0900

    Initial Commit

---
+++ hirakanaChanger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hirakanaChanger
+//
+//  Created by 溝口健太 on 2020/01/29.
+//  Copyright © 2020 溝口健太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
